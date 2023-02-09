a193d9db535e43fbd739dab18a1aebc365db57ca
parkjiyun98 <110899456+parkjiyun98@users.noreply.github.com>
2023-02-09 21:20:40 +0900

    Time: 10 ms (54.68%), Space: 62.2 MB (82.91%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
@@ -16,7 +16,7 @@
 class Solution {
     public int minDepth(TreeNode root) {
         if (root == null) return 0;
-        if (root.left == null && root.right == null) return 1;
+        //if (root.left == null && root.right == null) return 1;
         
         if(root.left == null) return 1 + minDepth(root.right);
         else if(root.right == null) return 1 + minDepth(root.left);
