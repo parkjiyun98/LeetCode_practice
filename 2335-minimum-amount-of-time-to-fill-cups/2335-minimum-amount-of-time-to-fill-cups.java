2609038fb05ecd4f9de28fd1722d4990aded6740
parkjiyun98 <110899456+parkjiyun98@users.noreply.github.com>
2023-02-13 10:42:19 +0900

    Time: 1 ms (83.82%), Space: 40.1 MB (47.48%) - LeetHub

--- 2335-minimum-amount-of-time-to-fill-cups/2335-minimum-amount-of-time-to-fill-cups.java
+++ 2335-minimum-amount-of-time-to-fill-cups/2335-minimum-amount-of-time-to-fill-cups.java
@@ -3,9 +3,7 @@ class Solution {
         Arrays.sort(amount);
         int count = amount[0] + amount[1] + amount[2];
         if(amount[0] + amount[1]> amount[2])  
-            return count/2+ count%2; 
-        else if(amount[0]==0 && amount[1]==0)  
-            return amount[2]; 
+            return count/2+ count%2;  
         else  
             return amount[2];  
     }
