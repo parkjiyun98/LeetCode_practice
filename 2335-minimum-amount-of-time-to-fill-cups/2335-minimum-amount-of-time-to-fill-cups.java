eb7d7aa2953cb3b97af30b8fe87f4a58ce183acc
parkjiyun98 <110899456+parkjiyun98@users.noreply.github.com>
2023-02-13 10:40:15 +0900

    Time: 1 ms (83.82%), Space: 40.3 MB (28.78%) - LeetHub

--- 2335-minimum-amount-of-time-to-fill-cups/2335-minimum-amount-of-time-to-fill-cups.java
+++ 2335-minimum-amount-of-time-to-fill-cups/2335-minimum-amount-of-time-to-fill-cups.java
@@ -5,7 +5,7 @@ class Solution {
         if(amount[0] + amount[1]> amount[2])  
             return count/2+ count%2; 
         else if(amount[0]==0 && amount[1]==0)  
-          return amount[2]; 
+            return amount[2]; 
         else  
             return amount[2];  
     }
