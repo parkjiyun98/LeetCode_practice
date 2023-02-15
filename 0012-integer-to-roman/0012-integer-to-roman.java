8559425e3203d4cb8b2fb06e1a4ac530893625cc
parkjiyun98 <110899456+parkjiyun98@users.noreply.github.com>
2023-02-15 11:34:22 +0900

    Time: 5 ms (72.34%), Space: 43.9 MB (19.55%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -1,5 +1,6 @@
 class Solution {
     public String intToRoman(int num) {
+        /*
         StringBuilder res = new StringBuilder();
         String[] roman ={"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
         int []values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
@@ -13,5 +14,35 @@ class Solution {
             i++;
         }
         return res.toString();
+        */
+        String answer = "";
+
+        for(int idx = 0; idx < num / 1000; idx++) answer = answer.concat("M");
+        num %= 1000;
+        for(int idx = 0; idx < num / 900; idx++) answer = answer.concat("CM");
+        num %= 900;
+        for(int idx = 0; idx < num / 500; idx++) answer = answer.concat("D");
+        num %= 500;
+        for(int idx = 0; idx < num / 400; idx++) answer = answer.concat("CD");
+        num %= 400;
+        for(int idx = 0; idx < num / 100; idx++) answer = answer.concat("C");
+        num %= 100;
+        for(int idx = 0; idx < num / 90; idx++) answer = answer.concat("XC");
+        num %= 90;
+        for(int idx = 0; idx < num / 50; idx++) answer = answer.concat("L");
+        num %= 50;
+        for(int idx = 0; idx < num / 40; idx++) answer = answer.concat("XL");
+        num %= 40;
+        for(int idx = 0; idx < num / 10; idx++) answer = answer.concat("X");
+        num %= 10;
+        for(int idx = 0; idx < num / 9; idx++) answer = answer.concat("IX");
+        num %= 9;
+        for(int idx = 0; idx < num / 5; idx++) answer = answer.concat("V");
+        num %= 5;
+        for(int idx = 0; idx < num / 4; idx++) answer = answer.concat("IV");
+        num %= 4;
+        for(int idx = 0; idx < num / 1; idx++) answer = answer.concat("I");
+
+        return answer;
     }
 }
