cc451c82bc84556078af863134af9cd65ca98224
parkjiyun98 <110899456+parkjiyun98@users.noreply.github.com>
2023-02-16 15:58:37 +0900

    Time: 9 ms (40.25%), Space: 47.5 MB (36.08%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
+		
+        ArrayList<int[]> overlap = new ArrayList<>();
+        int start = intervals[0][0], end = intervals[0][1];
+        
+        for(int idx = 1 ; idx< intervals.length ; ++idx) {
+        	if(intervals[idx][0] <= end) {
+        		end = Math.max(end,intervals[idx][1]);
+        	}else {
+        		overlap.add(new int[] {start,end});
+        		start = intervals[idx][0];
+        		end = intervals[idx][1];
+        	}
+        }
+        overlap.add(new int[] {start,end});
+        
+        int[][] answer = new int[overlap.size()][2];
+        for(int i=0; i<overlap.size() ; ++i) 
+        	answer[i] = overlap.get(i);
+        
+        return answer;
+    }
+}
