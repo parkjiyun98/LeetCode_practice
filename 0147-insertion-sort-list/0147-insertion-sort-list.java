b535869256da308fc3964e21c6977919aa1f163c
parkjiyun98 <110899456+parkjiyun98@users.noreply.github.com>
2023-02-16 17:54:28 +0900

    Time: 5 ms (66.38%), Space: 41.9 MB (52.38%) - LeetHub

--- 0147-insertion-sort-list/0147-insertion-sort-list.java
+++ 0147-insertion-sort-list/0147-insertion-sort-list.java
@@ -10,6 +10,7 @@
  */
 class Solution {
     public ListNode insertionSortList(ListNode head) {
+        /*
         if(head == null) return head;
         ListNode current = head.next;
         ListNode pre = head;
@@ -37,6 +38,27 @@ class Solution {
             }
         }
         return head;
-        
-    }
+        */
+        if(head == null || head.next == null) return head;
+        List<Integer> list = new LinkedList<>();
+        ListNode temp = head;
+        while(head != null){
+            list.add(head.val);
+            head = head.next;
+        }
+        int[] arr = list.stream().mapToInt(Integer::intValue).toArray();
+        Arrays.sort(arr);
+        return makeListNode(arr);
+
+        }
+        public ListNode makeListNode(int[] arr){
+            ListNode ans  = new ListNode();
+            ans.val = arr[0];
+            ListNode ansNext = ans;
+            for(int i = 1; i < arr.length; i++){
+                ansNext.next= new ListNode(arr[i]);
+                ansNext = ansNext.next;
+            }
+            return ans;
+        }
 }
