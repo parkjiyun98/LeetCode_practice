66d424c07bda884c31f58b46917580343599c907
parkjiyun98 <110899456+parkjiyun98@users.noreply.github.com>
2023-02-16 16:38:10 +0900

    Time: 0 ms (100.00%), Space: 41 MB (29.40%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,6 +1,7 @@
 class Solution {
     public void sortColors(int[] nums) {
         //Arrays.sort(nums);
+        /*
         int x = 0;
         int y = 0;
         for (int z = 0; z < nums.length; z++) {
@@ -8,6 +9,39 @@ class Solution {
             nums[z] = 2;
             if (v < 2) nums[y++] = 1;
             if (v == 0) nums[x++] = 0;
+        }
+        */
+        int x =0, y=0, z=0;
+        for(int i=0; i<nums.length; i++)
+        {
+            if(nums[i]==0)
+                x++;
+            else if(nums[i]==1)
+                y++;
+            else
+                z++;
+        }
+        
+        int i=0;
+        while(x>0)
+        {
+            nums[i]=0;
+            i++;
+            x--;
+        } 
+        
+        while(y>0)
+        {
+            nums[i]=1;
+            i++;
+            y--;
+        }
+        
+        while(z>0)
+        {
+            nums[i]=2;
+            i++;
+            z--;
         }
     }
 }
