a8fff001db5f81d2252bc0e7aa2526d70c40445a
parkjiyun98 <110899456+parkjiyun98@users.noreply.github.com>
2023-02-16 17:07:21 +0900

    Time: 21 ms (44.11%), Space: 41.8 MB (74.40%) - LeetHub

--- 0147-insertion-sort-list/0147-insertion-sort-list.java
+++ 0147-insertion-sort-list/0147-insertion-sort-list.java
@@ -10,6 +10,30 @@
  */
 class Solution {
     public ListNode insertionSortList(ListNode head) {
+        if( head == null ){
+			return head;
+		}
+		
+		ListNode helper = new ListNode(0); //new starter of the sorted list
+		ListNode cur = head; //the node will be inserted
+		ListNode pre = helper; //insert node between pre and pre.next
+		ListNode next = null; //the next node will be inserted
+		//not the end of input list
+		while( cur != null ){
+			next = cur.next;
+			//find the right place to insert
+			while( pre.next != null && pre.next.val < cur.val ){
+				pre = pre.next;
+			}
+			//insert between pre and pre.next
+			cur.next = pre.next;
+			pre.next = cur;
+			pre = helper;
+			cur = next;
+		}
+		
+		return helper.next;
+        /*
         ListNode result = new ListNode();
         ListNode previous = result;
         ListNode current = head;
@@ -25,5 +49,6 @@ class Solution {
           current = next;
         }
         return result.next;
+        */
     }
 }
