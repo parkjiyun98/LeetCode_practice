9088041fa8e0095c46d7b47334c129f1a8c44e7d
parkjiyun98 <110899456+parkjiyun98@users.noreply.github.com>
2023-02-16 13:40:02 +0900

    Time: 42 ms (55.33%), Space: 49.7 MB (46.59%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -5,7 +5,7 @@ class Solution {
             return result;
         }
         Arrays.sort(nums);
-        for (int i = 0; i < nums.length; i++) {
+        for (int i = 0; i < nums.length - 2; i++) {
             //결과값 중복 x 
             if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
