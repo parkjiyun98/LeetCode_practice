f758fcf5805ed8712441a8099046729a1bb55da9
parkjiyun98 <110899456+parkjiyun98@users.noreply.github.com>
2023-02-16 14:02:49 +0900

    Time: 12 ms (96.60%), Space: 42 MB (56.91%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -5,10 +5,11 @@ class Solution {
         int result = nums[0] + nums[1] + nums[2];
         for (int i=0;i<nums.length; i++)
         {
-            int left=i+1, right= nums.length - 1;
-            while(left<right)
+            int left = i+1;
+            int right = nums.length - 1;
+            while(left < right)
             {
-                int sum = nums[i]+nums[left]+nums[right];
+                int sum = nums[i] + nums[left] + nums[right];
                 if(Math.abs(sum-target) < Math.abs(result-target)) 
                     result = sum;
                 if(sum == target) 
