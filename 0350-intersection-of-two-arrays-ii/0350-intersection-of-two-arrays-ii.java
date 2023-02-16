09151816004e350314132b34e10f874f7ad5a260
parkjiyun98 <110899456+parkjiyun98@users.noreply.github.com>
2023-02-16 10:17:04 +0900

    Time: 3 ms (54.95%), Space: 42.2 MB (81.43%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -1,5 +1,6 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
+        /*
         List<Integer> list = new ArrayList<>();
         int i = 0, j = 0;
 
@@ -19,5 +20,32 @@ class Solution {
         }
 
         return list.stream().mapToInt(x -> x).toArray();
+        */
+        HashMap<Integer, Integer> map = new HashMap<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        int i; 
+            
+        for(i = 0; i < nums1.length; i++)
+        {
+            map.put(nums1[i], map.getOrDefault(nums1[i], 0)+1);
+        }
+
+        for(i = 0; i < nums2.length; i++)
+        {
+            if(map.containsKey(nums2[i]) && map.get(nums2[i]) > 0)
+            {                
+                list.add(nums2[i]);                        
+                map.put(nums2[i], map.get(nums2[i])-1);
+            }
+        }
+        int [] arr = new int[list.size()];
+
+        for(i = 0; i < list.size(); i++)
+        {
+            arr[i] = list.get(i);
+        }
+        
+        return arr;
     }
 }
