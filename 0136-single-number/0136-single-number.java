2126eab4f38a0dc6a56731900a142485b5d2e9f0
parkjiyun98 <110899456+parkjiyun98@users.noreply.github.com>
2023-02-21 10:47:22 +0900

    Time: 1231 ms (5.07%), Space: 42.5 MB (52.91%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,5 +1,6 @@
 class Solution {
     public int singleNumber(int[] nums) {
+        /*
         Map<Integer,Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             map.put(nums[i], map.getOrDefault(nums[i],0)+1);
@@ -10,5 +11,17 @@ class Solution {
             }
         }
         return 0;
+        */
+        int i=0;
+        ArrayList<Integer> list=new ArrayList<Integer>();
+        for( i=0;i<nums.length;i++){
+            if(list.contains(nums[i])){
+              int n=list.indexOf(nums[i]);
+             list.remove(n);
+            }else if(!list.contains(nums[i])){
+                list.add(nums[i]);
+            }                     
+        }
+         return  list.get(0);
     }
 }
